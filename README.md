c0fd41df3fc532670aaeafe0689e4c4fb1a04f5a
PradhyumShivhare <143643006+pradxnebula@users.noreply.github.com>
2023-11-03 18:08:36 +0530

    Added Files

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pure CSS3 Animation CAR running
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/aman4404/pen/RwwpjWa](https://codepen.io/aman4404/pen/RwwpjWa).
+
---
+++ script.js
@@ -0,0 +1,32 @@
+
+const car = document.querySelector('.car');
+const girlImage = document.querySelector('.girl-img');
+
+// Function to hide the girl image
+function hideGirlImage() {
+  girlImage.style.display = 'none';
+}
+
+
+// Function to show the girl image
+function showGirlImage() {
+  girlImage.style.display = 'inline';
+}
+var toggle = true;
+
+function girlHide() {
+  setInterval(() => {
+    toggle ? hideGirlImage() : showGirlImage();
+    toggle = !toggle;
+  }, 5000);
+}
+setInterval(() => {
+  document.querySelector('.loader-bg').style.display = 'none';
+}, 600);
+girlHide()
+showGirlImage();
+
+
+function switchFunction() {
+location.href="sign-up.html"
+}
